1f2f0fb6cb5a79f081da783284829c009539ccfb
Royal Lachinov <royalla@Royals-Air.ht.home>
2024-01-07 23:01:11 -0500

    Removed user's critical permissions

--- app/src/main/java/main/MapFragment.kt
+++ app/src/main/java/main/MapFragment.kt
@@ -7,8 +7,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import az.com.sheki.R
@@ -50,10 +48,5 @@ class MapFragment : Fragment() {
             }
             false
         }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.menu_share, menu)
-        super.onCreateOptionsMenu(menu, inflater)
     }
 }
